6a2a814361f109c986cbb7e42efb4b941fb42d70
kits26kat24 <kay.tijani@gmail.com>
2022-04-22 09:14:42 +0100

    first repo update

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "Tool-Server" {
-  ami           = "ami-08ca3fed11864d6bb"
-  instance_type = "t3.micro"
+  ami           = "ami-0850dc444c846e247"
+  instance_type = "t2.micro"
   key_name      = "kits26kat2486"
   subnet_id     = aws_subnet.public_subnet_Tool-Server.id
 
